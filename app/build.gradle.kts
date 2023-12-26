382ba1bf8258afbf853b70806c0e501cf2343303
morayl <sea.gang.moray@gmail.com>
2023-12-26 22:51:08 +0900

    Remove unnecessary code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,52 +3,21 @@ plugins {
     id("composenavigationargs.primitive.android.kotlin")
     id("composenavigationargs.primitive.android.compose")
     id("composenavigationargs.primitive.android.hilt")
-    id("composenavigationargs.primitive.android.firebase")
-    id("composenavigationargs.primitive.android.crashlytics")
     id("composenavigationargs.primitive.detekt")
-    id("composenavigationargs.primitive.kover.entrypoint")
-    id("composenavigationargs.primitive.android.osslicenses")
 }
 
 android {
     namespace = "com.morayl.composenavigationargs"
-    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.morayl.composenavigationargs"
-        minSdk = 26
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
@@ -62,12 +31,5 @@ dependencies {
     implementation(libs.androidxWindow)
     implementation(libs.kermit)
     implementation(libs.androidxSplashScreen)
-    implementation(libs.firebaseDynamicLinks)
     testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.test.ext.junit)
-//    androidTestImplementation(libs.espresso.core)
-//    androidTestImplementation(platform(libs.compose.bom))
-//    androidTestImplementation(libs.ui.test.junit4)
-//    debugImplementation(libs.ui.tooling)
-//    debugImplementation(libs.ui.test.manifest)
 }
--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -62,42 +62,6 @@ gradlePlugin {
             id = "composenavigationargs.primitive.android.roborazzi"
             implementationClass = "com.morayl.composenavigationargs.primitive.AndroidRoborazziPlugin"
         }
-        register("kotlinMpp") {
-            id = "composenavigationargs.primitive.kmp"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KmpPlugin"
-        }
-        register("kotlinMppIos") {
-            id = "composenavigationargs.primitive.kmp.ios"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KmpIosPlugin"
-        }
-        register("kotlinMppAndroid") {
-            id = "composenavigationargs.primitive.kmp.android"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KmpAndroidPlugin"
-        }
-        register("kotlinMppCompose") {
-            id = "composenavigationargs.primitive.kmp.compose"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KmpComposePlugin"
-        }
-        register("kotlinMppKtorfit") {
-            id = "composenavigationargs.primitive.kmp.ktorfit"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KmpKtorfitPlugin"
-        }
-        register("kotlinMppAndroidHilt") {
-            id = "composenavigationargs.primitive.kmp.android.hilt"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KmpAndroidHiltPlugin"
-        }
-        register("kotlinMppAndroidShowkase") {
-            id = "composenavigationargs.primitive.kmp.android.showkase"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KmpAndroidShowkasePlugin"
-        }
-        register("kotlinMppKotlinSerialization") {
-            id = "composenavigationargs.primitive.kmp.serialization"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KotlinSerializationPlugin"
-        }
-        register("koverEntryPoint") {
-            id = "composenavigationargs.primitive.kover.entrypoint"
-            implementationClass = "com.morayl.composenavigationargs.primitive.KoverEntryPointPlugin"
-        }
         register("detekt") {
             id = "composenavigationargs.primitive.detekt"
             implementationClass = "com.morayl.composenavigationargs.primitive.DetektPlugin"
