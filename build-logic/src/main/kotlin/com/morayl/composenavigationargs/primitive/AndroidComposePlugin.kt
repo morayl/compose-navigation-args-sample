af262a52f148ddefcc57edee39855f6160977f43
morayl <sea.gang.moray@gmail.com>
2023-12-27 15:12:42 +0900

    Remove material

--- build-logic/src/main/kotlin/com/morayl/composenavigationargs/primitive/AndroidComposePlugin.kt
+++ build-logic/src/main/kotlin/com/morayl/composenavigationargs/primitive/AndroidComposePlugin.kt
@@ -18,7 +18,6 @@ class AndroidComposePlugin : Plugin<Project> {
                 implementation(platform(libs.library("composeBom")))
                 implementation(libs.library("androidxCoreKtx"))
                 implementation(libs.library("composeUi"))
-                implementation(libs.library("composeMaterial"))
                 implementation(libs.library("composeMaterial3"))
                 implementation(libs.library("composeUiToolingPreview"))
                 implementation(libs.library("androidxLifecycleLifecycleRuntimeKtx"))
