5410301b253c7dcff2a3bf4c33a62b8bea3d4488
morayl <sea.gang.moray@gmail.com>
2023-12-27 16:23:19 +0900

    Fix bug

--- feature/search/src/main/java/com/morayl/composenavigationargs/feature/search/SearchTopViewModel.kt
+++ feature/search/src/main/java/com/morayl/composenavigationargs/feature/search/SearchTopViewModel.kt
@@ -2,32 +2,33 @@ package com.morayl.composenavigationargs.feature.search
 
 import androidx.lifecycle.ViewModel
 import com.morayl.composenavigationargs.core.model.SearchParameter
+import com.morayl.composenavigationargs.core.ui.ext.stateIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchTopViewModel @Inject constructor() : ViewModel() {
-    private val _parameterFlow: MutableStateFlow<String> = MutableStateFlow("no parameter")
-    val parameterFlow: StateFlow<String> = _parameterFlow.asStateFlow()
+    private val _parameterFlow: MutableStateFlow<SearchParameter?> = MutableStateFlow(null)
+    val parameterFlow: StateFlow<String> = _parameterFlow.map { it?.toString() ?: "no parameter" }.stateIn("")
 
-    private val _floatValueFlow: MutableStateFlow<String> = MutableStateFlow("no float")
-    val floatValueFlow: StateFlow<String> = _floatValueFlow.asStateFlow()
+    private val _floatValueFlow: MutableStateFlow<Float?> = MutableStateFlow(null)
+    val floatValueFlow: StateFlow<String> = _floatValueFlow.map { it?.toString() ?: "no float" }.stateIn("")
 
     fun updateParameter(parameter: SearchParameter) {
-        _parameterFlow.update { parameter.toString() }
+        _parameterFlow.update { parameter }
     }
 
     fun updateFloat(value: Float) {
-        _floatValueFlow.update { value.toString() }
+        _floatValueFlow.update { value }
     }
 
     fun clearResults() {
-        _parameterFlow.update { "no parameter" }
-        _floatValueFlow.update { "no float" }
+        _parameterFlow.update { null }
+        _floatValueFlow.update { null }
     }
 
 }
