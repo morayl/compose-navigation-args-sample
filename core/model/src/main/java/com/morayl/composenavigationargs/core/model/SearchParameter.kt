3118ff7f39c5808a8e0979880ac445cdcc5d8233
morayl <sea.gang.moray@gmail.com>
2023-12-27 15:46:11 +0900

    Implement screen result

--- core/model/src/main/java/com/morayl/composenavigationargs/core/model/SearchParameter.kt
+++ core/model/src/main/java/com/morayl/composenavigationargs/core/model/SearchParameter.kt
@@ -2,6 +2,7 @@ package com.morayl.composenavigationargs.core.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.time.LocalTime
 
 @Parcelize
-data class SearchParameter(val word: String, val number: Int, val order: Order) : Parcelable
+data class SearchParameter(val word: String, val number: Int, val order: Order, val time: LocalTime = LocalTime.now()) : Parcelable
