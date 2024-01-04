0e943c0114a4e05be40954564e59647ea7af1b00
morayl <sea.gang.moray@gmail.com>
2024-01-04 16:24:37 +0900

    Make navType to non null

--- app/src/main/java/com/morayl/composenavigationargs/toplevel/navType.kt
+++ app/src/main/java/com/morayl/composenavigationargs/toplevel/navType.kt
@@ -7,11 +7,11 @@ import androidx.navigation.navArgument
 import com.morayl.composenavigationargs.core.ui.navigation.ScreenArgs
 import kotlinx.serialization.json.Json
 
-internal inline fun <reified T : ScreenArgs> navScreenArgument(isNullableAllowed: Boolean = false): List<NamedNavArgument> {
-    return listOf(navArgument(ScreenArgs.KEY) { type = navType<T>(isNullableAllowed) })
+internal inline fun <reified T : ScreenArgs> navScreenArgument(): List<NamedNavArgument> {
+    return listOf(navArgument(ScreenArgs.KEY) { type = navType<T>(false) })
 }
 
-internal inline fun <reified T : ScreenArgs> navType(isNullableAllowed: Boolean = false): NavType<T> {
+private inline fun <reified T : ScreenArgs> navType(isNullableAllowed: Boolean = false): NavType<T> {
     return object : NavType<T>(isNullableAllowed = isNullableAllowed) {
         override fun put(bundle: Bundle, key: String, value: T) {
             bundle.putParcelable(key, value)
